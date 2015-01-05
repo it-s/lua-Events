559f04a7cb95a6cb8bb9b5572bc8518ffd9e0add
Eugene Trounev <eugene.trounev@gmail.com>
2015-01-04 20:34:28 -0500

    Make all the functions local

--- Events.lua
+++ Events.lua
@@ -1,3 +1,7 @@
+
+local insert = table.insert
+local remove = table.remove
+local ipairs = ipairs
 local Events = {}
 
 function Events.extend (base)
@@ -8,13 +12,13 @@ function Events.extend (base)
 		if type( func ) ~= "function" then return false end
 		if not self._events then self._events = {} end
 		if not self._events[eventType] then self._events[eventType] = {} end
-		table.insert( self._events[eventType], func )
+		insert( self._events[eventType], func )
 		return self
 	end
 	function base:removeEventListener(eventType, func)
 		if type( func ) ~= "function" then return false end
 		if not self._events then return false end
-		table.remove( self._events[eventType], func )
+		remove( self._events[eventType], func )
 		return self
 	end
 	function base:removeEventListeners(eventType)
@@ -33,12 +37,11 @@ function Events.extend (base)
 		if not self._events then return false end
 		-- FN.log ("Dispatching event " .. params.name .. " from " .. params.resourceName)
 		local events = self._events[params.name]
-		if events then
-			for i = 1, #events do
-				local event = events[i]
-				if type( event == "function" ) then event(params) end
-			end
-		end
+		if events and #events > 0 then
+                  for i, event in ipairs(#events)
+			if type( event == "function" ) then event(params) end
+                  end
+                end
 		return self
 	end
 
